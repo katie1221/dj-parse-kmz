b8fc45655ce475f56ac656e55e0ef3534d23dc0f
katie1221 <68039943+katie1221@users.noreply.github.com>
2025-09-15 10:29:30 +0800

    Add files via upload

---
+++ dj-point-demo/src/main/java/com/example/djpointdemo/common/FileTypeConstants.java
@@ -0,0 +1,28 @@
+package com.example.djpointdemo.common;
+
+/**
+ * @author qzz
+ * @date 2025/8/27
+ */
+public interface FileTypeConstants {
+
+    /**
+     * kmz 文件本质上是zip文件
+     */
+    String KMZ = "zip";
+
+    /**
+     * kml 文件
+     */
+    String KML = "kml";
+
+    /**
+     * wpml 文件
+     */
+    String WPML = "wpml";
+
+    /**
+     * 航线导入支持的文件类型
+     */
+    String ROUTE_SUPPORT_TYPE = "kml,zip";
+}
